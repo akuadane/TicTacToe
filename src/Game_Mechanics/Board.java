19dcfdcc839d961a60ca4adf030500eb2f09a985
akayou_adane <akuadane1205@gmail.com>
2023-04-27 15:25:06 +0300

    added toString to Board class

--- src/Game_Mechanics/Board.java
+++ src/Game_Mechanics/Board.java
@@ -7,12 +7,12 @@ public class Board {
     private Symbol[][] board;
     private Symbol turn;
 
-    public Board(){
+    public Board() {
         this.board = new Symbol[3][3];
         this.turn = Symbol.X;
     }
 
-    public void display(){
+    public void display() {
         for (Symbol[] symbols : board) {
             System.out.print("|");
             for (Symbol symb : symbols) {
@@ -25,13 +25,28 @@ public class Board {
         }
     }
 
-    public void makeMove(Move mv) throws InvalidMove{
+    public void makeMove(Move mv) throws InvalidMove {
         int r = mv.getR(), c = mv.getC();
 
-        if(board[r][c]!=null)
+        if (board[r][c] != null)
             throw new InvalidMove("Square already occupied."); // check if the square is empty
 
         board[r][c] = turn;
-        turn = (turn==Symbol.O)? Symbol.X : Symbol.O; // Changes the turn after making a move
+        turn = (turn == Symbol.O) ? Symbol.X : Symbol.O; // Changes the turn after making a move
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (Symbol[] symbols : board) {
+
+            for (Symbol symb : symbols) {
+                if (symb == null)
+                    str += "N";
+                else
+                    str += symb;
+            }
+        }
+        return str;
     }
 }
